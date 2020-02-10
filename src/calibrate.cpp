d5e9f817cd7465533bccc23869ae8eeddd763b67
ALIENK9 <alezanga@outlook.it>
2020-02-10 15:33:32 +0100

    Add calibration file

---
+++ src/calibrate.cpp
@@ -0,0 +1,100 @@
+#include <cmath>
+#include <experimental/filesystem>
+#include <vector>
+
+#include "IteratedLK.hpp"
+#include "TSPinstance.hpp"
+#include "TSPsolution.hpp"
+#include "utils/params.hpp"
+#include "utils/variadic_table.hpp"
+#include "utils/yaml_parser.hpp"
+
+class NullBuffer : public std::streambuf {
+ public:
+  int overflow(int c) { return c; }
+};
+
+NullBuffer _nullBuff = NullBuffer();
+std::ostream nullStream(&_nullBuff);
+
+using std::vector;
+namespace fs = std::experimental::filesystem;
+
+vector<unsigned int> Ks = {2, 100};
+vector<unsigned int> MNs{2, 5, 10};
+vector<unsigned int> BTs{2, 3, 4, 5};
+vector<unsigned int> IMDs{4, 5, 8, 10};
+vector<unsigned int> INTs{20, 60, 100, 200};
+
+void calibrateLK(Params P) {
+  unsigned int N = 90;
+  P.LK_iterations = 1;
+
+  TSPinstance inst;
+  inst.loadFromCsv("instances/tsp_90.csv");
+  double* costsM = inst.costMatrix();
+
+  fs::path logd = fs::current_path() / "files";
+  std::ofstream cal_log((logd / "calibration_out.txt").string(),
+                        std::ofstream::out);
+
+  double opt_val = 701.413;
+
+  VariadicTable<uint, uint, uint, uint, uint, double, double, double, int,
+                double, double>
+      table({"Max K", "Max neigh.", "Backtr. depth", "Intens. depth",
+             "Intens. tours", "Avg error (%)", "Best error (%)",
+             "Worst error (%)", "Avg impr. iterat.", "Tot time", "Score"});
+  double best_score = 0.0;
+  for (auto k : Ks) {
+    P.K = k;
+    for (auto max_neigh : MNs) {
+      P.max_neighbours = max_neigh;
+      for (auto back_depth : BTs) {
+        P.backtracking_threshold = back_depth;
+        for (auto int_min_depth : IMDs) {
+          P.intens_min_depth = int_min_depth;
+          for (auto int_n_tour : INTs) {
+            P.intens_n_tours = int_n_tour;
+            double avg_error = 0.0;
+            double best_error = std::numeric_limits<double>::infinity();
+            double worst_error = std::numeric_limits<double>::lowest();
+            double tot_time = 0.0;
+            int improving_it = 0;
+            int iterations = 10;
+            for (int iter = 0; iter < iterations; ++iter) {
+              auto [sol, time] = iterated_LK(P, N, costsM, nullStream);
+              double error = abs(sol.objVal - opt_val) / opt_val;
+              if (error < best_error) best_error = error;
+              if (error > worst_error) worst_error = error;
+              improving_it += sol.improving_iterations_lk;
+              avg_error += error;
+              tot_time += time;
+            }
+            avg_error = avg_error / iterations;
+            double score =
+                1.0 / ((tot_time * avg_error) + abs(log2(tot_time)) + 1.0);
+            if (score > best_score) best_score = score;
+            table.addRow({k, max_neigh, back_depth, int_min_depth, int_n_tour,
+                          avg_error * 100, best_error * 100, worst_error * 100,
+                          improving_it / iterations, tot_time, score});
+          }
+        }
+      }
+    }
+  }
+  cal_log << "Best score: " << best_score << "\n";
+  delete[] costsM;
+  table.print(cal_log);
+}
+
+int main() {
+  // Read config file
+  YamlP* parser = new YamlP;
+  parser->readConfig("src/config.yml");
+  Params P = parser->config();
+  delete parser;
+
+  calibrateLK(P);
+  return 0;
+}
